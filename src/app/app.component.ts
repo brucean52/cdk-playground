80a7132e3e89ff16644a13a28a5c977db9e5c9e5
Bruce An <bruce.an@marklogic.com>
2019-02-25 11:52:07 -0800

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,45 +6,5 @@ import { moveItemInArray } from '@angular/cdk/drag-drop';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  index: number;
   title = 'cdk-playground';
-
-  data = [
-    {
-      title: 'First Step',
-      description: 'This is the first step'
-    },
-    {
-      title: 'Second Step',
-      description: 'This is the second step'
-    },
-    {
-      title: 'Third Step',
-      description: 'This is the third step'
-    },
-    {
-      title: 'Fourth Step',
-      description: 'This is the fourth step'
-    }
-  ];
-
-  addStep(): void {
-    const newStep = {
-      title: 'New Step',
-      description: 'This is a new step'
-    }
-    this.data.push(newStep);
-  }
-  selectedStep(index: number) {
-    console.log('selected step index' , index);
-    this.index = index;
-  }
-  deleteStep(index: number) {
-    console.log('delete index');
-    this.data.splice(this.index, 1);
-  }
-  drop(output: any) {
-    moveItemInArray(this.data, output.previous, output.current);
-  }
 }
